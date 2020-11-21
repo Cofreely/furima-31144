588b0bb98f94f8099bd5f5ed494ae7653bc893a9
Cofreely <onomanz1015@gmail.com>
2020-11-21 18:02:41 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
+
   with_options presence: true do
-    validates :password, format: { with: /\A(?=.*?[a-zA-Z])[a-zA-Z\d]+\z/, message: 'Password include both letters and numbers' }
+    validates :nickname
+    validates :birth_date
+    validates :password, format: { with: /\A(?=.*?[a-zA-Z])[a-zA-Z\d]+\z/ }
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
   end
 end
